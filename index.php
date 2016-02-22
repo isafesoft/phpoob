9ed2e9dad87e327a77f21a9f346b47957f929c4f
Mark Xu <isafesoft@hotmail.com>
2016-02-22 01:49:10 -0800

    First commit shopProduct

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+* 
+*/
+class  ShopProduct 
+{
+	public $title;
+	public $pname;
+	public $vname;
+	public $price;
+
+	function __construct($title)
+	{
+		$this->title = 	$title;# code...
+
+	}
+
+	function getSummaryLine()
+	{
+		$base = "{$this->title}" . "pname";
+	}
+
+	/**
+	* 
+	*/
+	class CdProduct extends ShopProduct
+	{
+		public $playLength;
+		function __construct($title, $playLength)
+		{
+			parent::__construct($title);
+			$this->playLength = $playLength;
+		}
+
+		function getPlayLength()
+		{
+			return $this->playLength;
+		}
+	}
+
+
+}
